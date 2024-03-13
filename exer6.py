6f651a8d1b64ad09a4a622a515cbff78bb8dce71
evellyn cibele <128157952+evellyncibele@users.noreply.github.com>
2024-03-13 09:43:16 -0300

    Add files via upload

---
+++ exer6.py
@@ -0,0 +1,13 @@
+#dado um numero, classifique como positivo, negativo ou zero
+
+
+ # receber zero
+
+num= int(input('coloque um numero'))
+
+if num>0:
+    print('seu numero é positivo')
+elif num==0:
+    print('o seu numero é 0')
+else:
+    print('seu numero é negativo')
--- jogoadivinhacao.py
+++ jogoadivinhacao.py
@@ -2,18 +2,25 @@ print('*********************************')
 print('Bem vindo, ao JOGO DE ADIVINHAÇÃO')
 print('*********************************')
 
+#Definindo o número secreto
 numeroSecreto = 22
 
+#Definindo o número de tentativas
+numeroTentativas = 3
 
-chuteString = input('Digite um número')
+while(numeroTentativas > 0):
+    print('Tentativa restante: ', numeroTentativas)
 
-print('Você digitou o número', chuteString)
+#Recebendo o chute do jogador
+    chuteString = input('Digite um numero: ')
+    chute = int(chuteString)
 
-#chute = int(chuteString)
+#Declarando as condições
+    if (numeroSecreto == chute):
+        print('Você acertou!')
+    elif(chute>numeroSecreto):
+        print('Você errou! o numero é menor')
+    else:
+        print('você errou! o numero secreto é mior')
 
-if numeroSecreto == chute:
-    print('Você acertou!')
-elif(chute>numeroSecreto):
-    print('Você errou! o numero é menor')
-else:
-    print('você errou! o numero secreto é mior')
+    numeroTentativas = numeroTentativas - 1
