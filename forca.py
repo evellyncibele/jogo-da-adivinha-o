f859c70f0b1bdf5233acaa8b16f045f733ab4a9b
evellyn cibele <128157952+evellyncibele@users.noreply.github.com>
2024-05-29 08:55:36 -0300

    Add files via upload

---
+++ forca.py
@@ -0,0 +1,21 @@
+def jogar_forca():
+
+    print('*********************************')
+    print('Bem vindo, ao JOGO FORCA')
+    print('*********************************')
+    #Definir qual a palavra secreta
+    palavraSecreta = "tamandua"
+
+    enforcou = False
+    acertou = False
+
+    #Enquanto o jogador não se "enforcar" E não acertar a palavra, faça algo
+    while(not enforcou and not acertou):
+
+        chute = input("escolha uma letra!")
+        print("Escolha outra palavra")
+
+
+    print("Fim de jogo!")
+if(__name__ == "__main__"):
+    jogar_forca()
